7338cbf32318754650a7f5eb9c51d1d1e7c3c0bf
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-02-05 17:12:54 +0300

    убрала цену

--- Product.java
+++ Product.java
@@ -4,7 +4,7 @@ public class Product{
     String brand;
     double price;
     Product(){
-        price = 200;
+        
     }
 String displayInfo(){
     return String.format("%s - %s - %.2f", brand, name, price);
